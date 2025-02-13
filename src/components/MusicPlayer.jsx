f77ff340ec2e0a95704fecda0949bb6bec2bbe10
kimoochi <kimyu4606@gmail.com>
2025-02-14 02:59:23 +0800

    small changes

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,21 +1,22 @@
 'use client';
 
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react';
 
 export default function MusicPlayer({ isPlaying }) {
-  const audioRef = useRef(null)
+  const audioRef = useRef(null);
 
   useEffect(() => {
     if (isPlaying) {
-      audioRef.current?.play()
+      audioRef.current?.play().catch(error => console.log("Playback error:", error));
     } else {
-      audioRef.current?.pause()
+      audioRef.current?.pause();
     }
-  }, [isPlaying])
+  }, [isPlaying]);
 
   return (
-    <audio ref={audioRef} loop>
-      <source src="public/Rex_Orange_County_-_AMAZING_Lyrics.mp3" type="audio/mpeg" />
+    <audio ref={audioRef} loop controls>
+      <source src="/Rex_Orange_County_-_AMAZING_Lyrics.mp3" type="audio/mpeg" />
+      Your browser does not support the audio element.
     </audio>
-  )
+  );
 }
